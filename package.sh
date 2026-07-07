18d48921a278120570f66e39a03f6e3a755753b6
Fernando Chorney <github@djsbx.com>
2026-07-07 16:32:50 -0500

    Bump to v0.3.0

--- package.sh
+++ package.sh
@@ -59,9 +59,9 @@ EOF
     <key>CFBundleIdentifier</key>
     <string>com.fchorney.stepmaniax-gif-maker</string>
     <key>CFBundleVersion</key>
-    <string>0.2.0</string>
+    <string>0.3.0</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.2.0</string>
+    <string>0.3.0</string>
     <key>CFBundleExecutable</key>
     <string>stepmaniax-gif-maker</string>
     <key>CFBundleIconFile</key>
